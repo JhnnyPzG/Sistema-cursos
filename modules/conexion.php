d6b96269dd3fa25d390da21a52567ebf182fd1ee
JhnnyPzG <111128668+JhnnyPzG@users.noreply.github.com>
2022-10-06 16:43:59 -0500

    Update conexion.php

--- modules/conexion.php
+++ modules/conexion.php
@@ -1,11 +1,19 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
 
-$conexion = mysqli_connect("localhost", "root", "", "db_school");
+getenv('MYSQL_DBHOST') ? $db_host=getenv('MYSQL_DBHOST') : $db_host="localhost";
+getenv('MYSQL_DBPORT') ? $db_port=getenv('MYSQL_DBPORT') : $db_port="3306";
+getenv('MYSQL_DBUSER') ? $db_user=getenv('MYSQL_DBUSER') : $db_user="root";
+getenv('MYSQL_DBPASS') ? $db_pass=getenv('MYSQL_DBPASS') : $db_pass="";
+getenv('MYSQL_DBNAME') ? $db_name=getenv('MYSQL_DBNAME') : $db_name="";
 
-if (mysqli_connect_errno()) {
-	printf("Falló la conexión: %s\n", mysqli_connect_error());
-	exit();
-}
+if (strlen( $db_name ) === 0)
+  $conexion = new mysqli("$db_host:$db_port", $db_user, $db_pass);
+else 
+  $conexion = new mysqli("$db_host:$db_port", $db_user, $db_pass, $db_name);
+
+// Check connection
+if ($conexion->connect_error) 
+	die("Connection failed: " . $conn->connect_error);
 
 mysqli_set_charset($conexion, 'utf8');
