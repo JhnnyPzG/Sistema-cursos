8f03c0a088e181afa5408989952edb02425cdbf7
JhnnyPzG <111128668+JhnnyPzG@users.noreply.github.com>
2022-10-06 17:16:14 -0500

    Update conexion.php

--- modules/conexion.php
+++ modules/conexion.php
@@ -1,16 +1,15 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
 
-getenv('MYSQL_DBHOST') ? $db_host=getenv('MYSQL_DBHOST') : $db_host="localhost";
-getenv('MYSQL_DBPORT') ? $db_port=getenv('MYSQL_DBPORT') : $db_port="3306";
+getenv('MYSQL_DBHOST') ? $db_host=getenv('MYSQL_DBHOST') : $db_host="db";
 getenv('MYSQL_DBUSER') ? $db_user=getenv('MYSQL_DBUSER') : $db_user="root";
-getenv('MYSQL_DBPASS') ? $db_pass=getenv('MYSQL_DBPASS') : $db_pass="";
-getenv('MYSQL_DBNAME') ? $db_name=getenv('MYSQL_DBNAME') : $db_name="";
+getenv('MYSQL_DBPASS') ? $db_pass=getenv('MYSQL_DBPASS') : $db_pass="test";
+getenv('MYSQL_DBNAME') ? $db_name=getenv('MYSQL_DBNAME') : $db_name="db_school";
 
 if (strlen( $db_name ) === 0)
-  $conexion = new mysqli("$db_host:$db_port", $db_user, $db_pass);
+  $conexion = new mysqli($db_host, $db_user, $db_pass);
 else 
-  $conexion = new mysqli("$db_host:$db_port", $db_user, $db_pass, $db_name);
+  $conexion = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 // Check connection
 if ($conexion->connect_error) 
