86f505c21474593fe4e844bbce6823133ddfce00
JhnnyPzG <jpaz152@soyudemedellin.edu.co>
2022-10-28 08:54:21 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,10 +21,12 @@ resource "aws_instance" "app_server" {
     set -ex
     sudo yum update -y
     sudo yum install docker -y
+    sudo yum install docker-compose -y
     sudo service docker start
     sudo usermod -a -G docker ec2-user
     sudo docker pull jpzg/phpapache:latest
     sudo docker pull jpzg/sqlimage:8.0
+    sudo docker run -d -p 80:8000 jpzg/phpapache:latest
  EOF
  
  # https://github.com/hashicorp/terraform-provider-aws/issues/23315
