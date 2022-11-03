613e59335740765f54227f2ea59cf24870ec3561
JhnnyPzG <jpaz152@soyudemedellin.edu.co>
2022-11-03 09:42:07 -0500

    e

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,9 +23,9 @@ resource "aws_instance" "app_server" {
     sudo yum install docker git -y
     sudo service docker start
     sudo usermod -a -G docker ec2-user
-    sudo git clone https://github.com/JhnnyPzG/Sistema-cursos.git
-    sudo cd Sistema-cursos/
-    sudo docker-compose up 
+    sudo docker pull jpzg/phpapache:latest
+    sudo docker pull jpzg/sqlimage:8.0
+    sudo docker run -d -p 80:8000 jpzg/phpapache:latest
 
  EOF
  
